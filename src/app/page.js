40f3cde09ff849883a6eddced63ffd242734459e
brenno luan <brennoluan8530@gmail.com>
2026-06-15 19:30:15 -0300

    refactor: integrate logger for error handling and enhance layout with CSS module for improved styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,21 @@
 import { CardPost } from '@/components/CardPost';
+import logger from '@/logger';
+import styles from './page.module.css';
 
 async function getAllPosts() {
   const response = await fetch('http://localhost:3042/posts');
-
   if (!response.ok) {
-    console.log('Ops, alguma coisa correu mal');
+    logger.error('Ops, alguma coisa correu mal');
+    return [];
   }
-
+  logger.info('Posts obtidos com sucesso');
   return response.json();
 }
 
 export default async function Home() {
   const posts = await getAllPosts();
   return (
-    <main>
+    <main className={styles.grid}>
       {posts.map((post) => (
         <CardPost post={post} />
       ))}
